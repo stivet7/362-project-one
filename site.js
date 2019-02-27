3b5b87ab36f61d9d31a850febb46578351b90d04
Stive Tenorio <stivetenorio3520@yahoo.com>
2019-02-26 20:37:35 -0600

    Add email validation function

--- site.js
+++ site.js
@@ -3,3 +3,9 @@
 var html = document.querySelector('html');
 html.classList.remove('nojs');
 html.classList.add('js');
+
+// Email validity function
+function validate_email(value) {
+var email = clean_whitespace(value);
+return validate(email,/^[^@\s]+@[^@\s]+$/g);
+} 
