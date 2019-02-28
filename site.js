79a13b8743d43d8ff6d104192d6bffae91faf6e8
Stive Tenorio <stivetenorio3520@yahoo.com>
2019-02-28 00:47:47 -0600

    Added further email validation function

--- site.js
+++ site.js
@@ -5,7 +5,23 @@ html.classList.remove('nojs');
 html.classList.add('js');
 
 // Email validity function
-function validate_email(value) {
-var email = clean_whitespace(value);
+function validate_email(email) {
+var email = clean_whitespace(email);
 return validate(email,/^[^@\s]+@[^@\s]+$/g);
-} 
+}
+
+function validate() {
+  var $result = $("#result");
+  var email = $("#email").val();
+  $result.text("");
+
+  if (validate_email(email)) {
+    $result.text(email + " is valid :)");
+    $result.css("color", "green");
+  } else {
+    $result.text(email + " is not valid :(");
+    $result.css("color", "red");
+  }
+  return false;
+}
+$("#submit").bind("click", submit);
